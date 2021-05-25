07959cd7dd8a6a54978261f53eabe9ed886bd05f
danielpine <danielpine@sina.com>
2021-05-25 10:53:02 +0800

    update

--- travel.sql
+++ travel.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : local
-Source Server Version : 80019
+Source Server         : 1_local
+Source Server Version : 50731
 Source Host           : localhost:3306
 Source Database       : travel
 
 Target Server Type    : MYSQL
-Target Server Version : 80019
+Target Server Version : 50731
 File Encoding         : 65001
 
-Date: 2021-05-16 21:59:18
+Date: 2021-05-25 10:52:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -48,6 +48,57 @@ CREATE TABLE `carousel` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` bigint(20) NOT NULL,
+  `comment_type` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  `root_id` bigint(20) DEFAULT NULL,
+  `to_user_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKs1slvnkuemjsq2kj4h3vhx7i1` (`post_id`),
+  KEY `FKiqwjvq4ot3ap0vvmtfanse4fg` (`root_id`),
+  KEY `FK8400ne63906ddj3h6mqmboxp0` (`to_user_id`),
+  KEY `FK8kcum44fvpupyw6f5baccx25c` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES ('666', 'main', '阿达', '2021-05-21 17:41:42', '661', null, '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('667', 'sub', '对对对', '2021-05-21 17:41:46', null, '666', '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('668', 'next', '大大', '2021-05-21 17:41:49', null, '666', '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('673', 'main', '123', '2021-05-24 09:25:20', '661', null, '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('674', 'next', '13', '2021-05-24 09:25:23', null, '666', '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('675', 'next', '123', '2021-05-24 09:25:27', null, '666', '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('676', 'sub', '13', '2021-05-24 09:25:31', null, '673', '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('706', 'main', '22', '2021-05-24 17:48:12', '701', null, '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('724', 'main', 'hhh', '2021-05-24 19:15:45', '697', null, '1393432544690593792', '1393432544690593792');
+INSERT INTO `comment` VALUES ('728', 'main', '22', '2021-05-24 19:16:28', '725', null, '1393432544690593792', '1393432544690593792');
+
+-- ----------------------------
+-- Table structure for `favorite`
+-- ----------------------------
+DROP TABLE IF EXISTS `favorite`;
+CREATE TABLE `favorite` (
+  `id` bigint(20) NOT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKgmcgt9cgrsvdegu453v1tp98d` (`user_id`,`post_id`),
+  KEY `FKh20v4bwlpu57uv12dl7i2qipe` (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of favorite
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for `gallery`
 -- ----------------------------
 DROP TABLE IF EXISTS `gallery`;
@@ -69,13 +120,13 @@ CREATE TABLE `gallery` (
 -- ----------------------------
 DROP TABLE IF EXISTS `hibernate_sequence`;
 CREATE TABLE `hibernate_sequence` (
-  `next_val` bigint DEFAULT NULL
+  `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of hibernate_sequence
 -- ----------------------------
-INSERT INTO `hibernate_sequence` VALUES ('71');
+INSERT INTO `hibernate_sequence` VALUES ('758');
 
 -- ----------------------------
 -- Table structure for `hotel`
@@ -84,13 +135,13 @@ DROP TABLE IF EXISTS `hotel`;
 CREATE TABLE `hotel` (
   `id` varchar(255) NOT NULL,
   `addr` varchar(255) DEFAULT NULL,
-  `bed` int DEFAULT NULL,
-  `day` int DEFAULT NULL,
+  `bed` int(11) DEFAULT NULL,
+  `day` int(11) DEFAULT NULL,
   `img` varchar(255) DEFAULT NULL,
   `miaoshu` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `price` double DEFAULT NULL,
-  `star` int DEFAULT NULL,
+  `star` int(11) DEFAULT NULL,
   `startdate` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -110,7 +161,7 @@ CREATE TABLE `hotel_orders` (
   `payment` decimal(19,2) DEFAULT NULL,
   `paytime` datetime DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `qty` int DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
   `scenicid` varchar(255) DEFAULT NULL,
   `scenicname` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
@@ -128,9 +179,9 @@ CREATE TABLE `hotel_orders` (
 -- ----------------------------
 DROP TABLE IF EXISTS `image`;
 CREATE TABLE `image` (
-  `id` bigint NOT NULL,
+  `id` bigint(20) NOT NULL,
   `url` varchar(255) DEFAULT NULL,
-  `post_id` bigint DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKe2l07hc93u2bbjnl80meu3rn4` (`post_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -138,12 +189,22 @@ CREATE TABLE `image` (
 -- ----------------------------
 -- Records of image
 -- ----------------------------
-INSERT INTO `image` VALUES ('60', 'url', '59');
-INSERT INTO `image` VALUES ('62', 'url', '61');
-INSERT INTO `image` VALUES ('64', 'url', '63');
-INSERT INTO `image` VALUES ('66', 'url', '65');
-INSERT INTO `image` VALUES ('68', 'url', '67');
-INSERT INTO `image` VALUES ('70', 'url', '69');
+
+-- ----------------------------
+-- Table structure for `location`
+-- ----------------------------
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE `location` (
+  `id` bigint(20) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `lat` float DEFAULT NULL,
+  `lng` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of location
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `orders`
@@ -154,7 +215,7 @@ CREATE TABLE `orders` (
   `payment` decimal(19,2) DEFAULT NULL,
   `paytime` datetime DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `qty` int DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
   `scenicid` varchar(255) DEFAULT NULL,
   `scenicname` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
@@ -172,23 +233,44 @@ CREATE TABLE `orders` (
 -- ----------------------------
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
-  `id` bigint NOT NULL,
+  `id` bigint(20) NOT NULL,
   `post_time` datetime DEFAULT NULL,
-  `text` varchar(255) DEFAULT NULL,
+  `post_type` varchar(255) DEFAULT NULL,
+  `text` longtext,
+  `title` varchar(255) DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
   `user_id` varchar(255) DEFAULT NULL,
+  `topic` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `FKskbjob42y1o9hnesfkrnj7gbk` (`post_id`),
   KEY `FK72mt33dhhs48hf9gcqrq4fxte` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of post
 -- ----------------------------
-INSERT INTO `post` VALUES ('59', '2021-05-16 16:16:23', '123123', '1393432544690593792');
-INSERT INTO `post` VALUES ('61', '2021-05-16 16:22:51', '今天上海天气不错！！', '1393432544690593792');
-INSERT INTO `post` VALUES ('63', '2021-05-16 16:22:55', '今天上海天气不错！！', '1393432544690593792');
-INSERT INTO `post` VALUES ('65', '2021-05-16 16:22:55', '今天上海天气不错！！', '1393432544690593792');
-INSERT INTO `post` VALUES ('67', '2021-05-16 19:16:02', '8888', '1393432544690593792');
-INSERT INTO `post` VALUES ('69', '2021-05-16 19:16:38', '8888', '1393432544690593792');
+INSERT INTO `post` VALUES ('753', '2021-05-25 10:03:59', 'moments', '123', null, null, '1393432544690593791', null, 'normal');
+INSERT INTO `post` VALUES ('754', '2021-05-25 10:04:08', 'topic', '<p>22</p>', '22', null, '1393432544690593791', '22', 'normal');
+
+-- ----------------------------
+-- Table structure for `report`
+-- ----------------------------
+DROP TABLE IF EXISTS `report`;
+CREATE TABLE `report` (
+  `id` bigint(20) NOT NULL,
+  `report_type` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK9cymd8xyx2o6dovkrgxxxwuna` (`user_id`,`post_id`),
+  KEY `FKnuqod1y014fp5bmqjeoffcgqy` (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of report
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `scenic`
@@ -202,9 +284,9 @@ CREATE TABLE `scenic` (
   `miaoshu` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `price` double DEFAULT NULL,
-  `start` int DEFAULT NULL,
+  `start` int(11) DEFAULT NULL,
   `startdate` datetime DEFAULT NULL,
-  `stock` int DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -213,12 +295,40 @@ CREATE TABLE `scenic` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `thumb`
+-- ----------------------------
+DROP TABLE IF EXISTS `thumb`;
+CREATE TABLE `thumb` (
+  `id` bigint(20) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK3lwtwd0p0efxmbwg855dfksbj` (`user_id`,`post_id`),
+  KEY `FK38c4n3tepsl98shwfyhc5itvt` (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of thumb
+-- ----------------------------
+INSERT INTO `thumb` VALUES ('672', '1393432544690593792', '671');
+INSERT INTO `thumb` VALUES ('680', '1393432544690593792', '661');
+INSERT INTO `thumb` VALUES ('682', '1393432544690593792', '681');
+INSERT INTO `thumb` VALUES ('688', '1393432544690593792', '683');
+INSERT INTO `thumb` VALUES ('695', '1393432544690593792', '694');
+INSERT INTO `thumb` VALUES ('705', '1393432544690593792', '701');
+INSERT INTO `thumb` VALUES ('718', '1393432544690593792', '716');
+INSERT INTO `thumb` VALUES ('722', '1393432544690593792', '719');
+INSERT INTO `thumb` VALUES ('723', '1393432544690593792', '697');
+INSERT INTO `thumb` VALUES ('727', '1393432544690593792', '725');
+INSERT INTO `thumb` VALUES ('746', '1393432544690593791', '719');
+
+-- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` varchar(255) NOT NULL,
-  `age` int DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `mobile` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -233,4 +343,5 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1393432544690593792', '22', '12312@ww.com', '17612713011', 'user', '$2a$10$9DG9vMxxTWszn6sv7NhqiuY0BAPHX0SFEluFniHC56kUuE6O4VIN6', null, '女', '小雨点啊', '/static/upload/u=218375221,1552855610&fm=111&gp=0.jpg');
+INSERT INTO `user` VALUES ('1393432544690593792', '22', 'daniel@163.com', '17622123011', 'user', '$2a$10$9DG9vMxxTWszn6sv7NhqiuY0BAPHX0SFEluFniHC56kUuE6O4VIN6', null, '女', '一路向北丶', '/static/upload/00018.jpg');
+INSERT INTO `user` VALUES ('1393432544690593791', '22', '123@sina.com', '13122337756', 'user2', '$2a$10$9DG9vMxxTWszn6sv7NhqiuY0BAPHX0SFEluFniHC56kUuE6O4VIN6', null, '男', '月光', null);
